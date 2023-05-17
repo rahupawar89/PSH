5350f29ad2d2361c3a20c07e5be1a1bf98204f8d
hp <hp@LAPTOP-SU50L6MA>
2023-05-17 19:52:53 +0530

    [Rahul]: Add new class sample4

---
+++ src/psh1/sample4.java
@@ -0,0 +1,22 @@
+package psh1;
+
+public class sample4 
+{
+	public static void main(String[] args) 
+	{
+		sample4.Sname();
+		sample4.sname("Ganesh");
+		
+	}
+	public static void Sname() 
+	{
+		String sname="Rahul";
+		
+		System.out.println(sname);
+	}
+	public static void sname(String sname1) 
+	{
+		System.out.println(sname1);
+	}
+
+}
