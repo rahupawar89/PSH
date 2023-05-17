233b6743781fc910c812d775a614ebe6178b91e2
hp <hp@LAPTOP-SU50L6MA>
2023-05-17 19:44:58 +0530

    [rahul]: add new class sample3

---
+++ src/psh1/sample3.java
@@ -0,0 +1,15 @@
+package psh1;
+
+public class sample3 
+{
+	public static void main(String[] args) 
+	{
+		sample3.m1(10, 20);
+		
+	}
+	public static void m1(int a, int b) 
+	{
+		System.out.println(a+b);
+	}
+
+}
